0999be941d997458f162d7bc3d7a40dcffd34d82
Mariano Carbajo <104107401+MarianoCarbajo@users.noreply.github.com>
2022-05-03 11:21:05 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let numero: number = 0;
+while (numero <= 0) {
+  numero = prompt("Ingrese un número mayor a cero:");
+}
+if (numero % 2 == 0) {
+  console.log("El número " + numero + " es par");
+} else {
+  console.log("El número " + numero + " es impar");
+}
