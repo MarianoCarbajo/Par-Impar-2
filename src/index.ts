edf79a1ed30f37fb3ca59f44623053f2ec009689
Mariano Carbajo <104107401+MarianoCarbajo@users.noreply.github.com>
2022-05-12 11:30:40 -0300

    Corrección de errores El prompt lo declaré de tipo Number

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 let numero: number = 0;
 while (numero <= 0) {
-  numero = prompt("Ingrese un número mayor a cero:");
+  numero = Number(prompt("Ingrese un número mayor a cero:"));
 }
-if (numero % 2 == 0) {
+if (numero % 2 === 0) {
   console.log("El número " + numero + " es par");
 } else {
   console.log("El número " + numero + " es impar");
